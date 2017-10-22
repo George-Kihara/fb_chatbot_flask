395bafbc1922529dbdf9e909307db9fbcbe80867
GeorgeKihara <gegohcomp@gmail.com>
2017-10-23 00:07:44 +0300

    send button

--- app.py
+++ app.py
@@ -59,10 +59,8 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    if True:
-                        send_message(sender_id, "worked")
-                    else:
-                        send_message(sender_id, "yoo")
+                    
+                    send_message(sender_id, "yoo")
                         
                         
 
