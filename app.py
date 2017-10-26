5f620a8260bdad37e4e4ab2fff5ac0e0ce48d786
GeorgeKihara <gegohcomp@gmail.com>
2017-10-27 01:17:16 +0300

    overhaul

--- app.py
+++ app.py
@@ -88,25 +88,10 @@ def received_message(event):
 
     elif "attachments" in event["message"]:
         message_attachments = event["message"]["attachments"]   
-        send_text_message(sender_id, "Message with attachment received")
+        send_message(sender_id, "Message with attachment received")
 
 
-# Message event functions
-def send_text_message(recipient_id, message_text):
 
-    # encode('utf-8') included to log emojis to heroku logs
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text.encode('utf-8')))
-
-    message_data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
-    })
-
-    call_send_api(data)
 
 def send_greeting():
     #set greeting message on welcome screen
