cc0a5e4be2b3675405697fc4ec38af72dbda0aa6
GeorgeKihara <gegohcomp@gmail.com>
2017-10-27 11:05:19 +0300

    postbacks

--- app.py
+++ app.py
@@ -240,6 +240,55 @@ def send_button_community(recipient_id):
 
     call_send_api(data)
 
+def send_generic_message(recipient_id):
+    
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "rift",
+                        "subtitle": "Next-generation virtual reality",
+                        "item_url": "https://www.oculus.com/en-us/rift/",               
+                        "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                        "buttons": [{
+                            "type": "web_url",
+                            "url": "https://www.oculus.com/en-us/rift/",
+                            "title": "Open Web URL"
+                        }, {
+                            "type": "postback",
+                            "title": "Call Postback",
+                            "payload": "Payload for first bubble",
+                        }],
+                    }, {
+                        "title": "touch",
+                        "subtitle": "Your Hands, Now in VR",
+                        "item_url": "https://www.oculus.com/en-us/touch/",               
+                        "image_url": "http://messengerdemo.parseapp.com/img/touch.png",
+                        "buttons": [{
+                            "type": "web_url",
+                            "url": "https://www.oculus.com/en-us/touch/",
+                            "title": "Open Web URL"
+                        }, {
+                            "type": "postback",
+                            "title": "Call Postback",
+                            "payload": "Payload for second bubble",
+                        }]
+                    }]
+                }
+            }
+        }
+    })
+
+    log("sending template with choices to {recipient}: ".format(recipient=recipient_id))
+
+    call_send_api(data)
+
 def send_image_message(recipient_id):
     
     data = json.dumps({
