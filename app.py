79cae4ce233228352da6976645d126e50a3cbbff
GeorgeKihara <gegohcomp@gmail.com>
2017-10-29 01:41:10 +0300

    welcome greetings

--- app.py
+++ app.py
@@ -415,7 +415,7 @@ def received_postback(event):
 
     if payload == 'Get Started':
         # Get Started button was pressed
-        send_message(sender_id, "Welcome {} to bot store. You will find all facebook bots here.".format(user_details['first_name, last_name']))
+        send_message(sender_id, "Welcome {} {} to bot store. You will find all facebook bots here.".format(user_details['first_name'], user_details['last_name']))
         send_image_message(sender_id)
         send_button_message(sender_id)
     elif payload == 'Find a bot':
